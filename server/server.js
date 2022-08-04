169538d6e4bd1ce396a2102113b0ef9070e21597
Yasin ULUSOY <yasin.ulusoy@yahoo.com>
2022-08-04 15:43:50 +0300

    Server is created

---
+++ server/server.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const bp = require("body-parser");
+
+require("dotenv/config")
+const userRoutes = require( "../routes/userRoutes");
+const { errorHandler, notFound } = require("../middleware/errorMiddleware");
+
+app.use(cors());
+app.use(bp.json());
+// Error Handling middlewares
+
+
+
+app.use("/api/users", userRoutes);
+
+
+app.get("/", async (req, res) => {
+  res.send("Server is working");
+});
+
+mongoose.connect(
+  process.env.DB_CONNECTION,
+  { useNewUrlParser: true },
+  (err) => {
+    if (err) console.log(err);
+    else console.log("mongdb is connected");
+  }
+);
+
+
+
+
+const startServer = () => {
+    app.listen( 8080, console.log("Server is running"));
+ 
+};
+
+
+
+module.exports.startServer = startServer;
+
+
+
+
